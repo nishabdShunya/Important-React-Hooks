9d78382c3c042af5523598b4ae3e90e929cfef38
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-14 17:23:23 +0530

    Add. A3-T16-React Context & useContext Hook-With Solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// Write your code at relevant places in the code below:
-
 import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -30,13 +29,18 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,15 +1,13 @@
-// Write your code at relevant places in the code below:
+import React from "react";
 
-import React from 'react';
+import Navigation from "./Navigation";
+import classes from "./MainHeader.module.css";
 
-import Navigation from './Navigation';
-import classes from './MainHeader.module.css';
-
-const MainHeader = (props) => {
+const MainHeader = () => {
   return (
-    <header className={classes['main-header']}>
+    <header className={classes["main-header"]}>
       <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <Navigation />
     </header>
   );
 };
